f1039968c5d8fdb3a179cf9d0e44f37579f60582
maulik <krishna@krishaweb.net>
2019-11-12 14:20:17 +0530

    ok

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -48,9 +48,9 @@ class HomeController extends Controller
 
          
           
-        
+        File::makeDirectory(public_path().'/images/', 0777, true, true);
           
-        File::put(base_path().'/public/' . $imageName, base64_decode($image));
+        File::put(public_path().'/images/' . $imageName, base64_decode($image));
         
         
         
